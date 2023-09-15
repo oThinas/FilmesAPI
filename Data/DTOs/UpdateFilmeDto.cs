e226d33ac94f145306d53f8ab9d317667c21cd7b
oThinas <thiagomartinsprado@gmail.com>
2023-09-15 20:45:09 -0300

    initial commit

---
+++ Data/DTOs/UpdateFilmeDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.DTOs;
+
+public class UpdateFilmeDto
+{
+  [Required(ErrorMessage = "O campo Título é obrigatório")]
+  public string Titulo { get; set; }
+
+  [Required(ErrorMessage = "O campo Gênero é obrigatório")]
+  [StringLength(50, ErrorMessage = "O campo Gênero não pode ter mais de 50 caracteres")]
+  public string Genero { get; set; }
+
+  [Required]
+  [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 minutos e 600 minutos")]
+  public int Duracao { get; set; }
+}
---
+++ Profiles/FilmeProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using FilmesAPI.Data.DTOs;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Profiles;
+
+public class FilmeProfile : Profile
+{
+  public FilmeProfile()
+  {
+    CreateMap<CreateFilmeDto, Filme>();
+    CreateMap<UpdateFilmeDto, Filme>();
+    CreateMap<Filme, UpdateFilmeDto>();
+    CreateMap<Filme, ReadFilmeDto>();
+  }
+}
